408222c0749f81a84b7d3cf2956828e2382de3ea
Krakodil <krakodilgena79@bk.ru>
2022-04-08 19:51:18 +0300

    massivyone

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -9,11 +9,6 @@ public class Main {
         weights [1] = 2;
         weights [2] = 3;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(weights[i]);
-
-        }
-
         int firstWeights = weights [0];
         int secondWeights = weights [1];
         int thirdWeights = weights [2];
@@ -21,8 +16,13 @@ public class Main {
         System.out.println(secondWeights);
         System.out.println(thirdWeights);
 
-        int [] reversFirst = {1, 2, 3};
-        System.out.println(reversFirst);
+        System.out.println();
+
+        int [] reversFirst = {'1', '2', '3'};
+        for (int i = reversFirst.length - 1; i >= 0 ; i--) {
+            System.out.println(reversFirst[i]);
+        }
+        System.out.println();
 
         double [] weight = new double[3];
         weight [0] = 1.57;
@@ -37,29 +37,42 @@ public class Main {
         System.out.println(secondWeight);
         System.out.println(thirdWeight);
 
+        System.out.println();
+
         double [] reversSecond = {1.57, 7.654, 9.986};
-        System.out.println(reversSecond);
+        for (int i = reversSecond.length; i < 0; i--) {
+            System.out.println(reversSecond[i]);
+        }
+        System.out.println();
 
-        int [] length = new int [5];
-        length [0] = 16;
-        length [1] = 19;
-        length [2] = 21;
-        length [3] = 23;
-        length [4] = 25;
+        int [] power = new int [5];
+        power [0] = 16;
+        power [1] = 19;
+        power [2] = 21;
+        power [3] = 23;
+        power [4] = 25;
 
-        int firstLength = length [0];
-        int secondLength = length [1];
-        int thirdLength = length [2];
-        int forthLength = length [3];
-        int fifthLength = length [4];
+        int firstPower = power [0];
+        int secondPower = power [1];
+        int thirdPower = power [2];
+        int forthPower = power [3];
+        int fifthPower = power [4];
 
-        System.out.println(firstLength);
-        System.out.println(secondLength);
-        System.out.println(thirdLength);
-        System.out.println(forthLength);
-        System.out.println(fifthLength);
+        System.out.println(firstPower);
+        System.out.println(secondPower);
+        System.out.println(thirdPower);
+        System.out.println(forthPower);
+        System.out.println(fifthPower);
 
-        int reversThird = {16, 19, 21, 23, 25};
-        System.out.println(reversThird);
+        System.out.println();
+
+        char [] reversThird = {16, 19, 21, 23, 25};
+        for (int i = reversThird.length; i < 0; i--) {
+            System.out.println(reversThird[i]);
+        }
+        System.out.println();
+
+
+
     }
 }
